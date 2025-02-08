0195d1a63c6149ccaaca04b63d0f7ac2e99146f9
WaleedAlrefai <w.alrefai.3@gmail.com>
2025-02-08 13:57:13 +0300

    init projects

---
+++ projects/remote/src/app/remote-main/test1/test1.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-remote-test1',
+  standalone: false,
+  templateUrl: './test1.component.html',
+  styleUrl: './test1.component.scss'
+})
+export class Test1Component {
+
+}
